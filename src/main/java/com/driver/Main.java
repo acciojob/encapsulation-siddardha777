e3523445a1992b05189a79cdc233413502aa0467
siddardha777 <siddardhasiginam@gmail.com>
2024-07-25 11:01:46 +0530

    Encapsulation solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        r.setName("siddardha"); //name has private access so cant be used in another class
+    }
   
 }
